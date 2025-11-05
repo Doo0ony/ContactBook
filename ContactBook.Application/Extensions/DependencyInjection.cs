d2fd6b54b287f92ef344f3dc98c483cf86892e5e
danil.bespalov.00 <danil.bespalov.00@gmail.com>
2025-11-05 19:04:26 +0600

    feat: register AutoMapper in DI container

--- ContactBook.Application/Extensions/DependencyInjection.cs
+++ ContactBook.Application/Extensions/DependencyInjection.cs
@@ -1,6 +1,7 @@
 using ContactBook.Application.Interfaces.Services;
 using ContactBook.Application.Services;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 namespace ContactBook.Application.Extensions;
 
@@ -10,7 +11,19 @@ public static class DependencyInjection
     {
         services.AddScoped<IUserService, UserService>();
         services.AddScoped<IPhoneService, PhoneService>();
-        
+
+        return services;
+    }
+    
+    public static IServiceCollection AddMappingProfiles(this IServiceCollection services, IConfiguration builder)
+    {
+        // Configure AutoMapper with License Key
+        services.AddAutoMapper( cfg =>
+            cfg.LicenseKey = builder.GetConnectionString("AutoMapper"));
+            
+        // Add AutoMapper profiles
+        services.AddAutoMapper(cfg => { }, typeof(DependencyInjection).Assembly);
+
         return services;
     }
 }
