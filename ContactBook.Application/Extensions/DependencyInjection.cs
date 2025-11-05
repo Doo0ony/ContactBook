d1686f393f5ea1c0faa76377c70c5267e74feef0
danil.bespalov.00 <danil.bespalov.00@gmail.com>
2025-11-05 11:48:32 +0600

    chore: register UserService in DI container

---
+++ ContactBook.Application/Extensions/DependencyInjection.cs
@@ -0,0 +1,15 @@
+using ContactBook.Application.Interfaces.Services;
+using ContactBook.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ContactBook.Application.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<IUserService, UserService>();
+        
+        return services;
+    }
+}
