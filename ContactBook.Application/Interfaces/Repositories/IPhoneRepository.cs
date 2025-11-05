9f284360287d0417d2bd2fd5072e1b81ff8447a9
danil.bespalov.00 <danil.bespalov.00@gmail.com>
2025-11-05 19:15:21 +0600

    feat: add and implement GetPhonesByUserId method in IPhoneRepository

--- ContactBook.Application/Interfaces/Repositories/IPhoneRepository.cs
+++ ContactBook.Application/Interfaces/Repositories/IPhoneRepository.cs
@@ -1,4 +1,8 @@
+using ContactBook.Domain.Entities;
+
 namespace ContactBook.Application.Interfaces.Repositories;
 
-public interface IPhoneRepository : IGenericRepository<Domain.Entities.Phone>
-{}
+public interface IPhoneRepository : IGenericRepository<Phone>
+{
+    Task<IEnumerable<Phone>> GetPhonesByUserIdAsync(int userId, CancellationToken cancellationToken);
+}
--- ContactBook.Infrastructure/Repositories/PhoneRepository.cs
+++ ContactBook.Infrastructure/Repositories/PhoneRepository.cs
@@ -1,8 +1,18 @@
 using ContactBook.Application.Interfaces.Repositories;
 using ContactBook.Domain.Entities;
 using ContactBook.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContactBook.Infrastructure.Repositories;
 
 internal class PhoneRepository(AppDbContext context) : GenericRepository<Phone>(context), IPhoneRepository
-{}
+{
+    private readonly AppDbContext _context = context;
+
+    public async Task<IEnumerable<Phone>> GetPhonesByUserIdAsync(int userId, CancellationToken cancellationToken)
+    {
+        return await _context.Phones
+            .Where(p => p.UserId == userId)
+            .ToListAsync(cancellationToken);
+    }
+}
