50d90f2dc4d537b6c91d3e7406744fed301b847c
danil.bespalov.00 <danil.bespalov.00@gmail.com>
2025-11-05 17:02:34 +0600

    chore: create Fluent Validators for Phone Entity

---
+++ ContactBook.Api/Validators/Phone/CreatePhoneValidator.cs
@@ -0,0 +1,17 @@
+using FluentValidation;
+using ContactBook.Application.DTOs.Phone;
+
+namespace ContactBook.Api.Validators.Phone;
+
+public class CreatePhoneDtoValidator : AbstractValidator<CreatePhoneDto>
+{
+    public CreatePhoneDtoValidator()
+    {
+        RuleFor(x => x.PhoneNumber)
+            .NotEmpty().WithMessage("Phone number is required.")
+            .Matches(@"^\+?\d{10,15}$").WithMessage("Phone number is invalid.");
+
+        RuleFor(x => x.UserId)
+            .GreaterThan(0).WithMessage("UserId must be greater than 0.");
+    }
+}
---
+++ ContactBook.Api/Validators/Phone/UpdatePhoneValidator.cs
@@ -0,0 +1,21 @@
+using FluentValidation;
+using ContactBook.Application.DTOs.Phone;
+
+namespace ContactBook.Api.Validators.Phone;
+
+public class UpdatePhoneDtoValidator : AbstractValidator<UpdatePhoneDto>
+{
+    public UpdatePhoneDtoValidator()
+    {
+        RuleFor(x => x.Id)
+            .GreaterThan(0).WithMessage("Phone Id must be greater than 0.");
+
+        RuleFor(x => x.PhoneNumber)
+            .Matches(@"^\+?\d{10,15}$").WithMessage("Phone number is invalid.")
+            .When(x => !string.IsNullOrEmpty(x.PhoneNumber));
+
+        RuleFor(x => x.UserId)
+            .GreaterThan(0).WithMessage("UserId must be greater than 0.")
+            .When(x => x.UserId.HasValue);
+    }
+}
