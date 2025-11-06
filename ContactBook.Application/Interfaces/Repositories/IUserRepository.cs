7f1afe605601c0a41c7e3285560e1583f1dabc12
danil.bespalov.00 <danil.bespalov.00@gmail.com>
2025-11-06 15:07:18 +0600

    feat: add GetByEmailAsync method

--- ContactBook.Application/Interfaces/Repositories/IUserRepository.cs
+++ ContactBook.Application/Interfaces/Repositories/IUserRepository.cs
@@ -1,4 +1,8 @@
+using ContactBook.Domain.Entities;
+
 namespace ContactBook.Application.Interfaces.Repositories;
 
 public interface IUserRepository : IGenericRepository<Domain.Entities.User>
-{}
+{
+    Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken);
+}
--- ContactBook.Infrastructure/Repositories/UserRepository.cs
+++ ContactBook.Infrastructure/Repositories/UserRepository.cs
@@ -1,8 +1,15 @@
 using ContactBook.Application.Interfaces.Repositories;
 using ContactBook.Domain.Entities;
 using ContactBook.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContactBook.Infrastructure.Repositories;
 
 internal class UserRepository(AppDbContext context) : GenericRepository<User>(context), IUserRepository
-{}
+{
+    private readonly AppDbContext _context = context;
+    public async Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken)
+    {
+        return await _context.Users.FirstOrDefaultAsync(u => u.Email == email, cancellationToken);
+    }
+}
